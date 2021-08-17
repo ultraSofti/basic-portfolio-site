81d4a79322169b4af97fc388f033fb83b14f413e
Sylvester Akwaowo <ultrasoft101@gmail.com>
2021-08-17 22:21:47 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+
+<!--header section starts-->
+<header>
+
+
+</header>
+<!--header section ends-->
+<nav class="navbar">
+    <a href="#home">home</a>
+    <a href="#about">about</a>
+    <a href="#services">services</a>
+    <a href="#portfolio">portfolio</a>
+    <a href="#contact">contact</a>
+
+
+</nav>
+</body>
+</html>
